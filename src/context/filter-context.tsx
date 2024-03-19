810f8a42f98cceab83bc6fa096ab9f25f91c5b08
Fernando <fe.bonifacio@hotmail.com>
2024-03-19 09:53:48 -0300

    feat: componentes de pesquisas finalizados

--- src/context/filter-context.tsx
+++ src/context/filter-context.tsx
@@ -1,11 +1,14 @@
 "use client"
 import { FilterType } from "@/types/filter-types";
+import { PriorityTypes } from "@/types/priority.types";
 import { ReactNode, createContext, useState } from "react";
 
 export const FilterContext = createContext({
     search: '',
     page: 0,
     type: FilterType.ALL,
+    priority: PriorityTypes.NEWS, //SEMPRE MANDAR NOVO PRODUTO COMO PRIORIDADE
+    setPriority: (value: PriorityTypes) => {},
     setSearch: (value: string) => {},
     setPage: (value: number) => {},
     setType: (value: FilterType) => {},
@@ -19,9 +22,10 @@ export function FilterContextProvider({ children }: ProviderProps) {
     const [search, setSearch] = useState('');
     const [page, setPage] = useState(0);
     const [type, setType] = useState(FilterType.ALL)
+    const [priority, setPriority] = useState(PriorityTypes.NEWS)
 
     return (
-        <FilterContext.Provider value={{search, page, type, setSearch, setPage, setType}}>
+        <FilterContext.Provider value={{search, page, type, priority, setSearch, setPage, setType, setPriority}}>
 
             {children}
 
