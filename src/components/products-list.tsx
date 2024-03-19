8af21af2577a1f205738f43e9fd78fb7e36ff935
Fernando <fe.bonifacio@hotmail.com>
2024-03-19 14:08:21 -0300

    feat: buscando produtos da API

--- src/components/products-list.tsx
+++ src/components/products-list.tsx
@@ -1,1 +1,20 @@
+"use client"
+// aqui vai ficar o map e retornar os cards do produtos
 
+import { useProducts } from "@/hooks/userProdocts"
+
+interface ProductsListProps {
+
+}
+
+export function ProductsList(props: ProductsListProps) {
+
+    const { data } = useProducts();
+    //console.log(data);
+
+    return (
+        <>
+        </>
+    )
+
+}
