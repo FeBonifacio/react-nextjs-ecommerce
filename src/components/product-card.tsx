33c7d3636408169a30ecec0b66e6d379fd6efe03
Fernando <fe.bonifacio@hotmail.com>
2024-03-19 20:47:13 -0300

    feat: recebendo os datas da API e jogando em tela

---
+++ src/components/product-card.tsx
@@ -0,0 +1,74 @@
+import { styled } from "styled-components"
+
+interface ProductCardProps {
+    image: string,
+    title: string,
+    price: number,
+}
+
+const Card = styled.div`
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    flex-direction: column;
+    margin-top: 10px;
+
+    background: rgba(255, 255, 255, 0.4);
+    backdrop-filter: blur(10px);
+    border-radius: 0px 0px 4px 4px;
+
+    width: 256px;
+
+    img {
+        width: 256px;
+        height: 300px;
+    }
+
+    h3 {
+        font-weight: 300;
+        font-size: 16px;
+        line-height: 150%;
+        color: var(--text-dark-2);
+    }
+
+    p {
+        font-weight: 600;
+        font-size: 14px;
+        line-height: 150%;
+        color: var(--shapes-dark);
+        text-align: center;
+        margin-top: 8px;
+        margin-bottom: 8px;
+    }
+
+    div {
+        /* display: flex; */
+        /* align-items: start; */
+        /* justify-content: center; */
+        /* flex-direction: column; */
+        /* padding: 8px 0; */
+
+        > div {
+            width: 228px;
+            height: 1px;
+            margin: 8px 0;
+            padding: 0px;
+            background: var(--shapes);
+        }
+    }
+`
+
+export function ProductCard(props : ProductCardProps){
+    
+
+    return(
+        <Card>
+            <img src={props.image}/>
+            <div>
+                <h3>{props.title}</h3>
+                <div></div>
+                <p>{props.price}</p>
+            </div>
+        </Card>
+    )
+}
--- src/components/products-list.tsx
+++ src/components/products-list.tsx
@@ -2,10 +2,20 @@
 // aqui vai ficar o map e retornar os cards do produtos
 
 import { useProducts } from "@/hooks/userProdocts"
+import { ProductCard } from "./product-card";
+import styled from "styled-components";
 
 interface ProductsListProps {
 
 }
+
+const ListContainer = styled.div`
+    display: grid;
+    grid-template-columns: repeat(auto-fill, 256px);
+    grid-gap: 32px;
+    max-width: 100%;
+    background-color: 000;
+`
 
 export function ProductsList(props: ProductsListProps) {
 
@@ -13,8 +23,15 @@ export function ProductsList(props: ProductsListProps) {
     //console.log(data);
 
     return (
-        <>
-        </>
+        <ListContainer>
+            {data?.map(product => 
+                <ProductCard 
+                    key={product.id}
+                    title={product.name}
+                    price={product.price_in_cents}
+                    image={product.image_url}
+                />)}
+        </ListContainer>
     )
 
 }
