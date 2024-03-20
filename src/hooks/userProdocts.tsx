cfd798e54ea588e5bdd9971403cc49d8c3df5473
Fernando <fe.bonifacio@hotmail.com>
2024-03-20 15:17:44 -0300

    feat: buscar por categoria, preço, mais vendidos implementados

--- src/hooks/userProdocts.tsx
+++ src/hooks/userProdocts.tsx
@@ -1,32 +1,35 @@
-import { ProductsFetchResponse } from "@/types/ProductsFetchResponse";
+import { ProductsFetchResponse } from "@/types/products-response";
 import { useQuery } from "@tanstack/react-query";
 import axios, { AxiosPromise } from "axios";
+import { useFilter } from "./useFilter";
+import {  mountQuery } from "@/utils/graphql-filters";
+import { useDeferredValue } from "react";
 
 const API_URL = process.env.NEXT_PUBLIC_API_URL as string;
 
-const fetcher = (): AxiosPromise<ProductsFetchResponse> => {
-    return axios.post(
-        API_URL,
-        {
-            query: `query {
-                allProducts {
-                    id
-                    name
-                    price_in_cents
-                    image_url
-                }
-            }
-        `
-    })
+const fetcher = (query: string): AxiosPromise<ProductsFetchResponse> => {
+    return axios.post(API_URL,{ query })
 }
 
-export function useProducts() {
+export function useProducts(){
+    const { type, priority, search } = useFilter()
+    const searchDeferred = useDeferredValue(search)
+    const query = mountQuery(type, priority)
     const { data } = useQuery({
-        queryFn: fetcher,
-        queryKey: ['products']
+        queryFn: () => fetcher(query),
+        queryKey: ['products', type, priority]
     })
+
+    const products =  data?.data?.data?.allProducts
+
+    const filteredProducts = products?.filter(product => product.name.toLowerCase().includes(searchDeferred.toLowerCase()))
 
     return {
-        data: data?.data?.data?.allProducts //PromiseAxios duplica o date, ver ser tem jeito melhor para fazer
+        data: filteredProducts
     }
 }
+
+
+// implementa um hook useProducts que busca produtos de uma API GraphQL 
+// com base nos filtros e prioridades selecionados pelo usuário, e 
+// filtra esses produtos com base em uma pesquisa fornecida.
